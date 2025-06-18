4c3326ae61c45a9fc43685e3a5c3fb2e89b9b221
Tung Nguyen <nguyenductung03@gmail.com>
2025-06-19 00:41:58 +0700

    Writing actions to output.txt file

--- Basic_Programming/practice.c
+++ Basic_Programming/practice.c
@@ -24,90 +24,100 @@
 
 #define MAX_PEOPLE 100
 
-char   g_action_msg[17];
-char * g_type_all[] = {"anxin", "antrom", "congnhan"};
-
 typedef union income_t
 {
-    char s_val[7];
-    int i_val;
+    char    s_val[7];   // anxin, antrom
+    int     i_val;      // congnhan
     
 } income_t;
 
 typedef struct someone_t
 {
-    char type[9];
-    income_t income;
-    void (*p_action)(struct someone_t);
+    char        type[9];
+    income_t    income;
+    void        (*p_action)(struct someone_t);
     
 } someone_t;
 
 
-void cadge(someone_t person)
-{
-    strcpy(g_action_msg, "lam on lam phuoc");
-    printf("%s\n", g_action_msg);
-}
+char   g_action_msg[17];
+char * g_type_list[] = {"anxin", "antrom", "congnhan"};
 
-void stole(someone_t person)
-{
-    strcpy(g_action_msg, "!!!");
-    printf("%s\n", g_action_msg);
-}
 
-void work(someone_t person)
-{
-    sprintf(g_action_msg, "%d", person.income.i_val);
-    printf("%s\n", g_action_msg);
-}
-
-void create_person(someone_t * p_person, char * p_type)
-{
-    strcpy(p_person->type, p_type);
-    
-    if (strcmp(p_person->type, "anxin") == 0)
-    {
-        strcpy(p_person->income.s_val, "tuytam");
-        p_person->p_action = &cadge;
-    }
-    
-    else if (strcmp(p_person->type, "antrom") == 0)
-    {
-        strcpy(p_person->income.s_val, "henxui");
-        p_person->p_action = &stole;
-    }
-    
-    else if (strcmp(p_person->type, "congnhan") == 0)
-    {
-        p_person->income.i_val = 500000;
-        p_person->p_action = &work;
-    }
-}
+void cadge (someone_t person);
+void stole (someone_t person);
+void work  (someone_t person);
+void create_person (someone_t * p_person, char * p_type);
 
 
 int main()
 {
-    int counter;
     int number_people;
     someone_t people[MAX_PEOPLE];
+    FILE * p_file;
 
     printf("Nhap so nguoi: ");
     fflush(stdin);
     scanf("%d", &number_people);
     
-    for (counter = 0; counter < number_people; counter++)
+    for (int counter = 0; counter < number_people; counter++)
     {
-        create_person(&people[counter], g_type_all[rand() % 3]);
+        create_person(&people[counter], g_type_list[rand() % 3]);
     }
 
-    for (counter = 0; counter < number_people; counter++)
+
+    p_file = fopen("output.txt", "w");
+
+    for (int counter = 0; counter < number_people; counter++)
     {
         printf("%s ", people[counter].type);
         people[counter].p_action(people[counter]);
-
-
+        fprintf(p_file, "%s\n", g_action_msg);
     }
+
+    fclose(p_file);
     
     return 0;
 }
 
+
+void cadge (someone_t person)
+{
+    strcpy(g_action_msg, "lam on lam phuoc");
+    printf("%s\n", g_action_msg);
+}
+
+void stole (someone_t person)
+{
+    strcpy(g_action_msg, "!!!");
+    printf("%s\n", g_action_msg);
+}
+
+void work (someone_t person)
+{
+    sprintf(g_action_msg, "%d", person.income.i_val);
+    printf("%s\n", g_action_msg);
+}
+
+void create_person (someone_t * p_person, char * p_type)
+{
+    strcpy(p_person->type, p_type);
+    
+    if (0 == strcmp(p_person->type, "anxin"))
+    {
+        strcpy(p_person->income.s_val, "tuytam");
+        p_person->p_action = cadge;
+    }
+    
+    else if (0 == strcmp(p_person->type, "antrom"))
+    {
+        strcpy(p_person->income.s_val, "henxui");
+        p_person->p_action = stole;
+    }
+    
+    else if (0 == strcmp(p_person->type, "congnhan"))
+    {
+        p_person->income.i_val = 500000;
+        p_person->p_action = work;
+    }
+}
